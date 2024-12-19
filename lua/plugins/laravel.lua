800815930ce342660aeed30e8d0a4e8a25b2959b
Marc <coadoumarc@gmail.com>
2024-12-19 16:44:59 +0100

    add some laravel setup

--- lua/plugins/laravel.lua
+++ lua/plugins/laravel.lua
@@ -54,5 +54,14 @@ return {
       }
     end,
   },
+    {
+    -- Add the blade-nav.nvim plugin which provides Goto File capabilities
+    -- for Blade files.
+    "ricardoramirezr/blade-nav.nvim",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    ft = { "blade", "php" },
+  },
 }
 
