b194fe395fcf56ec1dd6c02ea4fdaa2b33aef5d3
Marc <coadoumarc@gmail.com>
2024-12-19 16:21:27 +0100

    init config with some laravel support

---
+++ lua/plugins/laravel.lua
@@ -0,0 +1,58 @@
+return {
+  {
+
+  "adalessa/laravel.nvim",
+  dependencies = {
+    "tpope/vim-dotenv",
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    "kevinhwang91/promise-async",
+  },
+  cmd = { "Laravel" },
+  keys = {
+    { "<leader>la", ":Laravel artisan<cr>" },
+    { "<leader>lr", ":Laravel routes<cr>" },
+    { "<leader>lm", ":Laravel related<cr>" },
+  },
+  event = { "VeryLazy" },
+  config = true,
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { { "pint", "php_cs_fixer" } },
+      },
+    },
+  },
+   {
+    -- Add a Treesitter parser for Laravel Blade to provide Blade syntax highlighting.
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "blade",
+        "php_only",
+        "html",
+        "javascript",
+      })
+    end,
+    config = function(_, opts)
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      require("nvim-treesitter.configs").setup(opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+    end,
+  },
+}
+
